8226b0a694df7ff4579f84cc4fb7e1ccf0eec183
itunz1 <stx_jose@hotmail.com>
2023-05-17 18:17:16 -0400

    final Update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Search pictures</p>
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
